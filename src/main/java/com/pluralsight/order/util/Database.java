89bebe7feebcbeb5ec5e5c51e50fa824a5cd9a9d
kpxics <kpxics@hotmail.com>
2023-04-20 02:20:59 -0400

    Database
    Completed Database.java

--- src/main/java/com/pluralsight/order/util/Database.java
+++ src/main/java/com/pluralsight/order/util/Database.java
@@ -5,14 +5,20 @@ import org.h2.tools.RunScript;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
  * Singleton class to get database connections
  */
 public class Database {
+
     private static Database instance = null;
     private static boolean isInitialized = false;
+    private static String url = "jdbc:h2:mem:orders;DB_CLOSE_DELAY=-1";
+    private static String user = "sa";
+    private static String password = "";
+
 
     /**
      * Private constructor
@@ -49,7 +55,7 @@ public class Database {
      * @throws SQLException In case of a database error
      */
     public Connection getConnection() throws SQLException {
-        Connection connection = null;
+        Connection connection = DriverManager.getConnection(url, user, password);
 
         if(!isInitialized && connection != null) {
             initializeDatabase(connection);
